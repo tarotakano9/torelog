448a88abcda87aefc9ec6d22f006bce4b24baa36
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2021-02-01 18:25:15 +0900

    ニックネームとパスワードだけで登録ができた

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,32 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:nickname]
+
+           #usernameを必須とする
+  validates_uniqueness_of :nickname
+  validates_presence_of :nickname
+
+  #usernameを利用してログインするようにオーバーライド
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      #認証の条件式を変更する
+      where(conditions).where(["nickname = :value", { :value => nickname }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+# No use email
+  def email_required?
+    false
+  end
+  def email_changed?
+    false
+  end
+  def will_save_change_to_email?
+    false
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2021_02_01_085858) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
     t.string "reset_password_token"
@@ -21,7 +20,6 @@ ActiveRecord::Schema.define(version: 2021_02_01_085858) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
