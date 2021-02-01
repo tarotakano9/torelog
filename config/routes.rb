0ea96193a3b8896141269888fdec93cad366bf30
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2021-02-01 17:08:37 +0900

    投稿までのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   # ホーム画面へのルーティング
   root 'home#index'
   resources :home, only: [:index]
+
+  # トレーニング記録へのルーティング
+  resources :trainings do
+    member do
+      get :submit_log
+    end
+  end
 end
