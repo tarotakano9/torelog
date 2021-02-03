17bd7f9238d4927f021b2337bdd6db3b73e99054
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2021-02-04 02:33:27 +0900

    セーブができたエラーメッセージ の日本語化

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,2 +1,34 @@
 class TrainingsController < ApplicationController
+  def index
+  end
+
+  def new
+    @day = Day.new
+  end
+
+  def create
+    @day = Day.new(day_params)
+    judge_target_exists
+    if @day.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def day_params
+    params.require(:day).permit(
+      :date, :target_exists, :chest, :shoulder, :tricep, :bicep, :back, :abdominal, :leg
+    ).merge(user_id: current_user.id)
+  end
+
+  def judge_target_exists
+    if @day.chest || @day.shoulder || @day.tricep || @day.bicep || @day.back || @day.abdominal || @day.leg
+      @day.target_exists = true
+    else
+      @day.target_exists = nil
+    end
+  end
 end
