75d7f0132b006fedf846ef158a6b4ad9e9893ca8
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2021-02-12 22:17:54 +0900

    確認機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # ホーム画面へのルーティング
   root 'home#index'
-  resources :home, only: [:index]
+  resources :home, only: [:index, :show]
 
   # トレーニング記録へのルーティング
   resources :trainings do
