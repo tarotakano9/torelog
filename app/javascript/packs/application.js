07ffadc941a4f26700c481cd22ebb6654adfcec4
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2021-02-16 19:28:46 +0900

    レイアウト周りの修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("form/remove_btn")
-import '../css/application.css'
+import '../src/style.scss'
 
 require('jquery')
+require("uikit")
 require("cocoon")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
